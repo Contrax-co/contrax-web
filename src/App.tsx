ea54b9b3686d8820d49985bdef0269371ffdf1ff
Huzaifa Asif <huzaifa@theaweb.net>
2021-10-29 12:08:20 +0500

    React Suite Library Removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home';
-// import default style
-// import 'rsuite/styles/index.less'; // or 'rsuite/dist/rsuite.min.css'
-import 'rsuite/dist/rsuite.min.css'
 
 function App() {
   return (

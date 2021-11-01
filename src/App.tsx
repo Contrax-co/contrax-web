5897f7fc46fb56ebcb4928464abed1030f9d3850
Huzaifa Asif <huzaifa@theaweb.net>
2021-11-01 17:05:55 +0500

    header component and routing work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Dashboard from './pages/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+       <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/dashboard'>
+              <Dashboard />
+            </Route>
+        </Switch>
+    </Router>
   );
 }
 

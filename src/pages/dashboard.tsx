9ee90478eb99ed7425f5e0a85277700e69ad194e
Huzaifa Asif <huzaifa@theaweb.net>
2021-11-04 14:40:11 +0500

    Dashboard Header Work, Page Setup, Tab-bar for Key Metrics

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Navigationbar from '../components/Navigationbar';
 import Header from '../components/Header';
 
 export default function dashboard() {
     return (
-        <div>
+        <>
+            <Navigationbar />
             <Header />
-        </div>
+            
+            <div className="container">
+                <nav>
+                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                        <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Key Metrics</button>
+                    </div>
+                </nav>
+                <div className="tab-content" id="nav-tabContent">
+                    <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+
+        </>    
     )
 }

62866325b2c6994514a4943641aa5bbe8eefcf3a
Huzaifa Asif <huzaifa@theaweb.net>
2021-11-12 18:53:36 +0500

    Create Token Form Work done

--- src/pages/createToken.tsx
+++ src/pages/createToken.tsx
@@ -1,17 +1,70 @@
 import React from 'react'
 import Navigationbar from '../components/Navigationbar';
 import BottomBar from '../components/bottomBar/BottomBar';
+import { PageTitle, PageSubTitle, Title, Desc } from "../components/text/Text";
 
 export default function createToken() {
     return (
         <>
              <Navigationbar />
-             <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-12">
-
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-12">
-
+             <div className="container h-100">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-sm-12 mx-auto my-auto">
+                        <PageTitle value={'No coding required. '} variant={'dark'} />
+                        <PageTitle value={'Create your own tokens with one click!'} variant={'dark'} />
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <form className="px-4 py-4 my-5 formBackgroundDesign">
+                        <div className="row">
+                            <div className="col-lg-12 col-md-12 col-sm-12 my-2">
+                                <Title variant={'dark'}  value={'Enter Token Parameters:'}></Title>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12 my-2">
+                                <Desc value={'Token Symbol'} variant={'dark'} />
+                                <input type="text" placeholder="1-16 Characters" className="form-control" id="tokenSymbol" name="tokenSymbol"  />
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12 my-2">
+                                <Desc value={'Token Supply'} variant={'dark'} />
+                                <input type="text" placeholder="0-99,999,999,999,999,999" className="form-control" id="tokenSypply" name="tokenSypply" />
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12 my-2">
+                                <Desc value={'Token Name'} variant={'dark'} />
+                                <input type="text" placeholder="1-64 Characters" className="form-control" id="tokenName" name="tokenName" />
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12 my-2">
+                                <Desc value={'Decimals'} variant={'dark'} />
+                                <input type="text" placeholder="0-18" className="form-control" id="decimals" name="decimals" />
+                            </div>
+                            <div className="col-lg-12 col-md-12 col-sm-12 mt-3 mb-2">
+                                <Title variant={'dark'}  value={'Special Features:'}></Title>
+                            </div>
+                            <div className="col-lg-10 col-md-10 col-sm-10 my-2">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <Title variant={'dark'}  value={'Burn'} className="inlineDisplay mx-2"></Title>
+                                <Desc className="form-check-label" value={'A percentage of tokens will be sent to the burn address for each on-chain transfer'} variant={'dark'} />
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-2 my-2">
+                                <input type="text" placeholder="0.1%" className="form-control" id="burnPercent" name="burnPercent" />
+                            </div> 
+                            <div className="col-lg-10 col-md-10 col-sm-10 my-2">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <Title variant={'dark'}  value={'Trading Fees'} className="inlineDisplay mx-2"></Title>
+                                <Desc className="form-check-label" value={'A percentage of tokens will be sent to the creators address for each on-chain transfer'} variant={'dark'} />
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-2 my-2">
+                                <input type="text" placeholder="0.1%" className="form-control" id="tradingFeePercent" name="tradingFeePercent" />
+                            </div> 
+                            <div className="col-lg-12 col-md-12 col-sm-12 my-2">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <Title variant={'dark'}  value={'Supports Supply Increase'} className="inlineDisplay mx-2"></Title>
+                                <Desc className="form-check-label" value={'Allows the creator to issue additional tokens after the token creation'} variant={'dark'} />
+                            </div>
+                        </div>    
+                        <div className="row justify-content-center  mx-5 mt-3">
+                            <button type="submit" className="btn btn-primary">Create a token</button>
+                        </div>     
+                        </form>
+                    </div>
                 </div>
              </div>
              <BottomBar />

689a113e710cc086afb94de9cf3104cf2aa2278c
AwaisRai <arai@gfyag.com>
2021-11-17 17:40:03 +0500

    form components created

---
+++ src/components/form/Form.styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/macro';
+
+import * as colors from '@colors';
+import * as typo from '@typography';
+
+const noForwardProps = [];
+
+export const StyledInput = styled('input', {
+  shouldForwardProp: (prop) => !noForwardProps.includes(prop),
+})((props) => ({
+  ...(props.variant === 'light' && { color: colors.titleLight }),
+  ...(props.variant === 'dark' && { color: colors.titleDark }),
+  ...(typo.Title),
+})
+);
+
+export const StyledCheckbox = styled('input', {
+  shouldForwardProp: (prop) => !noForwardProps.includes(prop),
+})((props) => ({
+  ...(props.variant === 'light' && { backgroundColor: colors.titleLight }),
+  ...(props.variant === 'dark' && { backgroundColor: colors.titleDark }),
+  ...(typo.Title),
+  float: 'left',
+  marginRight: '0.6rem',
+})
+);

3b23075a09e0b9830ad09b11819ec2a114c5abe4
Huzaifa Asif <huzaifa@theaweb.net>
2021-11-19 16:41:04 +0500

    PR Changes done - move color to central file

--- src/components/chart/PieChart.tsx
+++ src/components/chart/PieChart.tsx
@@ -1,51 +1,52 @@
 import React from 'react'
 import { render } from "react-dom";
 import { Chart } from "react-google-charts";
+import { chartColorBlue, chartColorYellow, chartTextGreyColor } from "../../theme/colors"
 
 const pieOptions = {
-    title: "",
-    pieHole: 0.6,
-    slices: [
-          {
-            color: "#007fad"
-          },
-          {
-            color: "#e9a227"
-          }
-    ],
-    legend: {
-      position: "bottom",
-      alignment: "center",
-      textStyle: {
-        color: "233238",
-        fontSize: 14
-      }
+  title: "",
+  pieHole: 0.6,
+  slices: [
+    {
+      color: chartColorBlue
     },
-    tooltip: {
-      showColorCode: true
-    },
-    chartArea: {
-      left: 0,
-      top: 0,
-      width: "100%",
-      height: "80%"
-    },
-    fontName: "Roboto"
+    {
+      color: chartColorYellow
+    }
+  ],
+  legend: {
+    position: "bottom",
+    alignment: "center",
+    textStyle: {
+      color: chartTextGreyColor,
+      fontSize: 14
+    }
+  },
+  tooltip: {
+    showColorCode: true
+  },
+  chartArea: {
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "80%"
+  },
+  fontName: "Roboto"
 };
 
 export default function PieChart() {
 
-    return (
+  return (
     <div className="App">
-        <Chart
-          chartType="PieChart"
-          data={[["Title", "Supply"], ["Your Tokens", 64000000], ["Other Tokens", 36000000]]}
-          options={pieOptions}
-          graph_id="PieChart"
-          width={"100%"}
-          height={"300px"}
-          legend_toggle
-        />
-      </div>
-    )
+      <Chart
+        chartType="PieChart"
+        data={[["Title", "Supply"], ["Your Tokens", 64000000], ["Other Tokens", 36000000]]}
+        options={pieOptions}
+        graph_id="PieChart"
+        width={"100%"}
+        height={"300px"}
+        legend_toggle
+      />
+    </div>
+  )
 }

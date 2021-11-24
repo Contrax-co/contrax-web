0d23c14e313d30979c1ae28d0dd8e7ee51d21f1e
AwaisRai <arai@gfyag.com>
2021-11-24 16:07:17 +0500

    routing setup done

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,41 +1,12 @@
 import Button from './button/Button';
 import { PageTitle, Desc, PageSubTitle, Title, Text } from "./text/Text";
-import Onboard from 'bnc-onboard';
-import Web3 from 'web3';
-import { setUserSession } from '../store/localstorage';
+
 import ethereum from "../images/ethereum.png";
 import arbitrum from "../images/arbitrum.png";
 import uniswap from "../images/uniswap.png";
 
-let web3
-const onboard = Onboard({
-  dappId: process.env.REACT_APP_DAPP_ID,  // [String] The API key of Blocknative
-  networkId: 3,  // [Integer] The Ethereum network ID your Dapp uses.
-  subscriptions: {
-    wallet: wallet => {
-      web3 = new Web3(wallet.provider)
-    }
-  }
-});
-
 export default function banner() {
-    async function ConnectWallet() {
-    const walletSelected = await onboard.walletSelect();
-    if (walletSelected) {
-      const readyToTransact = await onboard.walletCheck();
-      if (readyToTransact) {
-        const currentState = await onboard.getState()
-        setUserSession({
-          address: currentState.address,
-          appNetworkId: currentState.appNetworkId,
-          balance: currentState.balance,
-          mobileDevice: currentState.mobileDevice,
-          network: currentState.network,
-        });
-        window.location.href="/dashboard"
-      }
-    }
-  }
+ 
 
   return (
     <div>
@@ -54,7 +25,7 @@ export default function banner() {
                 <img src={arbitrum} className='brand-logo' alt='arbitrum' />
                 <img src={uniswap} className='brand-logo' alt='uniswap' />
               </div>
-              <Button label={'Explore Docs'} />
+              <Button label={'Explore Docs'}  />
             </div>
             {/* <div className="col-lg-6 col-sm-12 mx-auto my-auto">
               <PageTitle value={'Contrax Wallet'} variant={'light'} />

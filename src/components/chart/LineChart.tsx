ad775cbb45287895e424b7ac85754069b85bc772
Huzaifa Asif <huzaifa@theaweb.net>
2021-11-26 10:09:11 +0500

    Exchange Screen Pass Dynamic Data to Line chart

--- src/components/chart/LineChart.tsx
+++ src/components/chart/LineChart.tsx
@@ -19,27 +19,27 @@ const chartOptions = {
     fontName: "Roboto"
 };
 
-export const data = [
-  ["Date / Time", "Price"],
-  ["09:40 AM", 22.4],
-  ["09:45 AM", 13.5],
-  ["09:49 AM", 19.9],
-  ["10:14 AM", 25],
-  ["10:44 AM", 21],
-  ["10:59 AM", 28],
-  ["11:36 AM", 14],
-];
+// export const data = [
+//   ["Date / Time", "Price"],
+//   ["09:40 AM", 22.4],
+//   ["09:45 AM", 13.5],
+//   ["09:49 AM", 19.9],
+//   ["10:14 AM", 25],
+//   ["10:44 AM", 21],
+//   ["10:59 AM", 28],
+//   ["11:36 AM", 14],
+// ];
 
-
-export default function PieChart() {
-
+export default function LineChart(props:any) {
+  const {chartData} = props;
+  console.log("chartData ",chartData)
     return (
     <div className="App">
        <Chart
         chartType="Line"
         width="100%"
         height="400px"
-        data={data}
+        data={chartData}
         options={chartOptions}
       />
       </div>
--- src/pages/exchange.tsx
+++ src/pages/exchange.tsx
@@ -7,23 +7,71 @@ export default function Exchange() {
     const [day, setDay] = useState('active')
     const [week, setWeek] = useState('')
     const [month, setMonth] = useState('')
+    const [ChartData, setChartData] = useState([
+        ["Date / Time", "Price"],
+        ["09:40 AM", 22.4],
+        ["09:45 AM", 13.5],
+        ["09:49 AM", 19.9],
+        ["10:14 AM", 25],
+        ["10:44 AM", 21],
+        ["10:59 AM", 28],
+        ["11:36 AM", 14],
+      ])
 
     function changeState(state: any){
-        console.log("changeState: ", state);
+        // console.log("changeState: ", state);
         if(state == 'day'){
             setDay('active');
             setWeek(''); 
             setMonth('');
+            setChartData([
+                ["Date / Time", "Price"],
+                ["09:40 AM", 22.4],
+                ["09:45 AM", 13.5],
+                ["09:49 AM", 19.9],
+                ["10:14 AM", 25],
+                ["10:44 AM", 21],
+                ["10:59 AM", 28],
+                ["11:36 AM", 14],
+              ])
         }
         if(state == 'week'){
             setDay('');
             setWeek('active'); 
             setMonth('');
+            setChartData([
+                ["Date / Time", "Price"],
+                ["9/11/21 10:23 AM", 12.4],
+                ["10/11/21 12:45 PM", 33.5],
+                ["11/11/21 09:49 AM", 23.9],
+                ["12/11/21 10:14 AM", 18],
+                ["13/11/21 10:44 AM", 24],
+                ["14/11/21 10:59 AM", 28],
+                ["15/11/21 11:36 AM", 15],
+              ])
         }
         if(state == 'month'){
             setDay('');
             setWeek(''); 
             setMonth('active');
+            setChartData([
+                ["Date / Time", "Price"],
+                ["1/11/21 10:23 AM", 12.4],
+                ["2/11/21 12:45 PM", 33.5],
+                ["4/11/21 09:49 AM", 23.9],
+                ["6/11/21 10:14 AM", 18],
+                ["10/11/21 10:44 AM", 24],
+                ["11/11/21 10:59 AM", 28],
+                ["14/11/21 11:36 PM", 15],
+                ["16/11/21 12:36 AM", 24],
+                ["17/11/21 01:36 AM", 13],
+                ["19/11/21 03:36 PM", 27],
+                ["21/11/21 11:33 PM", 35],
+                ["23/11/21 08:36 PM", 38],
+                ["24/11/21 10:36 AM", 24],
+                ["26/11/21 05:36 PM", 42],
+                ["27/11/21 09:36 PM", 41],
+              ])
         }
     }
 
@@ -53,7 +101,7 @@ export default function Exchange() {
                                 </ul>
                             </div>
                         </div>
-                        <LineChart />
+                        <LineChart chartData={ChartData} />
                     </div>
                     <div className="col-lg-4 col-md-4 col-sm-12">
 

486a9ca20770dd9dd464ed63973d1ef514ac603e
Huzaifa Asif <huzaifa@theaweb.net>
2021-12-01 15:09:56 +0500

    Pool Detail Screen Routing and Setup Done

---
+++ src/pages/poolDetail.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import BottomBar from '../components/bottomBar/BottomBar'
+import Navigationbar from '../components/Navigationbar'
+
+export default function PoolDetail() {
+
+    return (
+        <div>
+            <Navigationbar />
+            <div className="container">
+            </div>
+            <BottomBar />
+        </div>
+    )
+}
+

ff6eb03f0226036b1495bace5c436c26177a9bf7
Soheeb <soheeb@gmail.com>
2021-12-27 12:36:19 -0600

    logos sizing and spacing fixed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -29,12 +29,10 @@ export default function Footer() {
         <Row>
           <Col size="6">
           <a href='https://discord.gg/rDdvyeWAtt'><Image src={discordLogo} alt='Discord' /></a>
-            <a href='https://twitter.com/Contrax_Finance'><Image className='m-5 mt-3 mb-3' src={twitterLogo} alt='Twitter' /></a>
-  
-            <a href='https://t.me/Contrax'><Image src={telegramLogo} alt='Telegram' /></a> 
-            <a href='https://medium.com/contrax-defi'><Image src={mediumLogo} alt='Medium' /></a> 
-            <a href='https://github.com/Contrax-co'><Image src={githubLogo} alt='GitHub' /></a> 
-
+          <a href='https://twitter.com/Contrax_Finance'><Image className='m-4 mt-1 mb-1' src={twitterLogo} alt='Twitter' /></a>
+          <a href='https://t.me/Contrax'><Image className='m-1 mt-1 mb-1' src={telegramLogo} alt='Telegram' /></a> 
+          <a href='https://medium.com/contrax-defi'><Image className='m-4 mt-1 mb-1' src={mediumLogo} alt='Medium' /></a> 
+          <a href='https://github.com/Contrax-co'><Image className='m-1 mt-1 mb-1' src={githubLogo} alt='GitHub' /></a> 
           </Col>
           <Col size="6" className='text-right'>
             {/* <Button primary size='sm' className='mt-3'>Get Started</Button> */}

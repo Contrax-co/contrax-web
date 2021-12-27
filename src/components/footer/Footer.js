e5710a2ece46fc4ce9b4d4d21b2dfe3456ca71af
Soheeb <soheeb@gmail.com>
2021-12-27 11:16:35 -0600

    logos and links in but wrong size for footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,6 +7,9 @@ import { StyledBox } from './Footer.styles';
 import footerLogo from "../../images/logo-footer.png";
 import discordLogo from "../../images/discord.png";
 import twitterLogo from "../../images/twitter.png";
+import telegramLogo from "../../images/telegram.png";
+import githubLogo from "../../images/github.png";
+import mediumLogo from "../../images/medium.png";
 import { Image } from '../image/Image';
 // import Button from '../button/Button';
 
@@ -27,11 +30,11 @@ export default function Footer() {
           <Col size="6">
           <a href='https://discord.gg/rDdvyeWAtt'><Image src={discordLogo} alt='Discord' /></a>
             <a href='https://twitter.com/Contrax_Finance'><Image className='m-5 mt-3 mb-3' src={twitterLogo} alt='Twitter' /></a>
-            {/* Logos to be added and updated. 
+  
             <a href='https://t.me/Contrax'><Image src={telegramLogo} alt='Telegram' /></a> 
             <a href='https://medium.com/contrax-defi'><Image src={mediumLogo} alt='Medium' /></a> 
             <a href='https://github.com/Contrax-co'><Image src={githubLogo} alt='GitHub' /></a> 
-            */}
+
           </Col>
           <Col size="6" className='text-right'>
             {/* <Button primary size='sm' className='mt-3'>Get Started</Button> */}

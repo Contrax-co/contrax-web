1359f66014f69215cee0591c4aa84b285d45f78e
Muhammad Usman Shamas <47473401+muhammad-usman56@users.noreply.github.com>
2022-08-17 00:59:12 +0500

    UI fixes with new Create Poo UIl

--- src/pages/CompoundEarn/compoundEarn.tsx
+++ src/pages/CompoundEarn/compoundEarn.tsx
@@ -15,6 +15,7 @@ import * as colors from '../../theme/colors';
 import { Badge } from '../../components/badge/Badge';
 import Icon from '../../components/icon/Icon';
 import ApprovalModal from './components/ApprovalModal';
+import swal from 'sweetalert';
 const axios = require('axios')
 export default function CompoundEarn() {
   const [volume, setVolume] = useState('active')
@@ -37,38 +38,89 @@ export default function CompoundEarn() {
       depositeTokens: "25"
     }
   ]
+  let usdData = [
+   
+    {
+      time: "2021/12/03 12:53:51",
+      paidCurrencyMedia: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=002",
+      receiveCurrencyMedia: "https://cryptologos.cc/logos/tether-usdt-logo.png?v=002",
+      price: "USDC - Tether",
+      apy: "0.008%",
+      tvl: "0.48",
+      deposite: "$50",
+      depositeTokens: "25"
+    }
+  ]
+
+
+
+
   return (
     <div>
       <Navigationbar />
       <Container className=' mt-4 pt-1'>
         {
           swapsData.map((item, index) => (
-      
+
             <Row className='compound-card'>
 
-             
-               <Col size="2"> {item.price} </Col>
-              <Col size="3">
-              APY<br />
+              <Col size="2"> <Image style={{ width: '20%', height: '20%' }} src={item.paidCurrencyMedia} /> <Image style={{ width: '20%', height: '20%' }} src={item.receiveCurrencyMedia} />
+              </Col>
+              <Col size="2"> {item.price} </Col>
+              <Col size="2">
+                APY<br />
                 {item.apy}
               </Col>
-              <Col size="2">
-              tvl <br />
+              <Col size="1">
+                tvl <br />
                 {item.tvl}
               </Col>
               <Col size="2">
                 Total Deposit  <br />
-               {item.deposite}
+                {item.deposite}
               </Col>
               <Col size="1">
-                <Button size='sm' onClick={() => { }} >Harvest</Button>
+                <Button size='sm' onClick={() => {  swal("That functionality not connected ."); }}  >Harvest</Button>
               </Col>
-              <Col size="1">
-                <Button primary size='sm' data-bs-toggle="modal" data-bs-target="#approvalModal" onClick={() => { }} >Withdrawal</Button>
+              <Col size="2">
+                <Button primary size='sm'  onClick={() => {   swal("That functionality not connected .");}} >Withdrawal</Button>
               </Col>
             </Row>
           ))
         }
+
+{
+          usdData.map((item, index) => (
+
+            <Row className='compound-card'>
+
+              <Col size="2"> <Image style={{ width: '20%', height: '20%' }} src={item.paidCurrencyMedia} /> <Image style={{ width: '20%', height: '20%' }} src={item.receiveCurrencyMedia} />
+              </Col>
+              <Col size="2"> {item.price} </Col>
+              <Col size="2">
+                APY<br />
+                {item.apy}
+              </Col>
+              <Col size="1">
+                tvl <br />
+                {item.tvl}
+              </Col>
+              <Col size="2">
+              
+              </Col>
+              <Col size="1">
+                {/* <Button size='sm' onClick={() => { }} >Harvest</Button> */}
+              </Col>
+              <Col size="2">
+                <Button primary size='sm' data-bs-toggle="modal" data-bs-target="#approvalModal" onClick={() => { }}  >Deposit</Button>
+              </Col>
+            </Row>
+          ))
+        }
+
+
+
+
         <ApprovalModal totalTokens={8.0456345} />
       </Container>
       <BottomBar />
--- src/pages/createPool.tsx
+++ src/pages/createPool.tsx
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navigationbar from '../components/Navigationbar/Navigationbar';
 import BottomBar from '../components/bottomBar/BottomBar';
-import { Title, H1, H3, B1 } from "../components/text/Text";
+import { Title, H1, H3, B1, Link, Desc, H2 } from "../components/text/Text";
 import Button, { ButtonGroupRadio } from "../components/button/Button";
 import { Form, FormInput } from "../components/form/Form";
 import DropdownInput from '../components/form/dropdownInput/DropdownInput';
-import TokenDrop from '../components/form/dropdownInput/tokenDrop';
 import { Col, Container, Row } from '../components/blocks/Blocks';
 import * as colors from '../theme/colors';
+import TokenModal from '../components/TokenModal';
+import StatsCard from '../components/statsCard/StatsCard';
+import { StyledDropBtn, StyledInput } from '../components/form/dropdownInput/DropdownInput.styles';
+import { Image } from '../components/image/Image';
+import PriceCounter from '../components/PriceCounter';
 import { gql, useMutation, useQuery } from '@apollo/client';
-import { getUserSession, setSelectedToken } from '../store/localstorage';
+import { ethers } from "ethers";
+import { getUserSession } from '../store/localstorage';
+import swal from 'sweetalert';
+import tokenlogo from '../images/tokenlogo.png'
+import TokenModal1 from '../components/TokenModal1';
 import abi from '../config/sushiswap.json';
 import ercabi from '../config/erc20.json'
-import { ethers } from "ethers"
-import swal from 'sweetalert';
+import factory from '../config/factory.json'
 const axios = require('axios')
-
-
-
 const FETCH = gql`
 query MyQuery($chainId:String!,$userwallet:String!) {
     tokens(where: {chainId: {_like: $chainId}, userwallet: {_like: $userwallet}}) {
@@ -35,17 +39,33 @@ query MyQuery($chainId:String!,$userwallet:String!) {
 `;
 
 
-
-
 export default function CreatePool() {
   const { ethereum } = window;
+  const currencies = [
+    { title: 'ETH', subTitle: 'Ethereum' },
+    { title: 'BTC', subTitle: 'Bitcoin' },
+    { title: 'USDT', subTitle: 'Tether' },
+    { title: 'SOL', subTitle: 'Solana' },
+    { title: 'ABT', subTitle: 'Arcblock' },
+    { title: 'GTS', subTitle: 'GT STAR' },
+  ]
+
+
+
+  const [tokenOne, setTokenOne] = useState<any | null>(null);
+  const [tokenTwo, setTokenTwo] = useState<any | null>(null);
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [minValue, setMinValue] = useState(1);
+  const [maxValue, setMaxValue] = useState(1);
+  const [tokenOneAmount, setTokenOneAmount] = useState('');
+  const [tokenTwoAmount, setTokenTwoAmount] = useState('');
+
+
+
   const [dtoken, setDTokens] = useState<any[]>([])
   const [wallet, setWallet] = useState()
   const [values, setValues] = useState([]);
-  const [tokenA, setTokenA] = useState();
-  const [tokenB, setTokenB] = useState();
-  const [amountAvalue, setAmountA] = useState();
-  const [amountBvalue, setAmountB] = useState();
+
   const { data, loading, error } = useQuery(FETCH, {
     variables: {
       chainId: "421611",
@@ -71,97 +91,6 @@ export default function CreatePool() {
 
   })
 
-  useEffect(() => {
-    getApiDetails();
-  }, [])
-
-
-  const TokenAaddressA = async (item: any, isTokenA: boolean) => {
-    console.log(item, isTokenA)
-    setTokenA(item)
-  }
-
-  const TokenAaddressB = async (item: any, isTokenA: boolean) => {
-    console.log(item, isTokenA)
-    setTokenB(item)
-  }
-
-  const amountA = async (value: any) => {
-    console.log(value)
-    setAmountA(value)
-  }
-  const amountB = async (value: any) => {
-    console.log(value)
-    setAmountB(value)
-  }
-
-
-
-  const createPool = async () => {
- console.log(tokenA);
-    console.log(tokenB);
-    console.log(amountAvalue);
-    console.log(amountBvalue);
-
-
-
-    const contractAddress = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
-    const contractABI = abi;
-    const tokenABI = ercabi.abi
-
-    const tokenAddress: any = tokenA;
-    const tokenAddressb: any = tokenB;
-
-    const amount1:any = amountAvalue
-    const amount2:any = amountBvalue
-
-if(amount1 === amount2){
-  const amount1min:any = amount1-1;
-  const amount2min:any = amount2-1;
-
-  const amountIn1 = ethers.utils.parseEther(amount1.toString());
-  const amountIn2 = ethers.utils.parseEther(amount2.toString());
-
-  const amount1Min = ethers.utils.parseEther(amount1min.toString());
-  const amount2Min = ethers.utils.parseEther(amount2min.toString());
-
-  const time = Math.floor(Date.now() / 1000) + 200000;
-  const deadline = ethers.BigNumber.from(time);
-
-   const userAddress = wallet;
-
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  const signer = provider.getSigner();
-  const router = new ethers.Contract(contractAddress, contractABI, signer);
-  const TOKEN = new ethers.Contract(tokenAddress, tokenABI, signer);
-  const TOKENB = new ethers.Contract(tokenAddressb, tokenABI, signer);
-
-  await TOKEN.approve(contractAddress,amountIn1);
-  await TOKENB.approve(contractAddress,amountIn2);
-
-
-
-  const hx = await router.addLiquidity(tokenAddress, tokenAddressb, amountIn1,
-    amountIn2,
-    amount1Min,
-    amount2Min, userAddress,deadline, {
-    gasLimit: 250000
-  });
-
-const hash = hx.wait()
-console.log(hash);
-
-swal("Create Pool Transaction is in Process", "Please Follow Metamask ");
-}else{
-  swal("TokenA Amount  and TokenB Amount Should be same ");
-}
-}
-
-   
-
-
-
-
   const getApiDetails = async () => {
     try {
       const result = await axios.post(
@@ -179,112 +108,216 @@ swal("Create Pool Transaction is in Process", "Please Follow Metamask ");
         })
       // console.log(result.data.data.tokens)
       setDTokens(result.data.data.tokens)
-
+      console.log(dtoken)
 
     } catch (error) {
       console.error(error);
     }
   }
+  useEffect(() => {
+    const tempData = getUserSession();
+    console.log(tempData);
 
-  console.log(dtoken)
+    if (tempData) {
+      const walletData = JSON.parse(tempData)
+      setWalletAddress(walletData.address);
+    }
+  }, [])
+
+
+  useEffect(() => {
+    getApiDetails();
+  }, [])
+
+  async function handleCreatePool() {
+    console.log(tokenOneAmount, tokenTwoAmount, tokenOne.tokenaddress, tokenTwo.id)
+    const contractAddress = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+    const factoryAddress = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
+    const contractABI = abi;
+    const tokenABI = ercabi.abi
+    const factoryABI = factory;
+    const tokenAddress: any = tokenOne.tokenaddress;
+    const tokenAddressb: any = tokenTwo.id;
+    console.log(tokenAddress, tokenAddressb)
+    const amount1: any = tokenOneAmount
+    const amount2: any = tokenTwoAmount
+
+    if (amount1 === amount2) {
+      const amount1min: any = amount1 - 1;
+      const amount2min: any = amount2 - 1;
+
+      const amountIn1 = ethers.utils.parseEther(amount1.toString());
+      const amountIn2 = ethers.utils.parseEther(amount2.toString());
+
+      const amount1Min = ethers.utils.parseEther(amount1min.toString());
+      const amount2Min = ethers.utils.parseEther(amount2min.toString());
+
+      const time = Math.floor(Date.now() / 1000) + 200000;
+      const deadline = ethers.BigNumber.from(time);
+
+      const userAddress = wallet;
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const router = new ethers.Contract(contractAddress, contractABI, signer);
+      const factory = new ethers.Contract(factoryAddress, factoryABI, signer);
+      const TOKEN = new ethers.Contract(tokenAddress, tokenABI, signer);
+      const TOKENB = new ethers.Contract(tokenAddressb, tokenABI, signer);
+
+      await factory.createPair(tokenAddress, tokenAddressb)
+
+
+      await TOKEN.approve(contractAddress, amountIn1);
+      await TOKENB.approve(contractAddress, amountIn2);
+
+
+
+      const hx = await router.addLiquidity(tokenAddress, tokenAddressb, amountIn1,
+        amountIn2,
+        amount1Min,
+        amount2Min, userAddress, deadline, {
+        gasLimit: 300000
+      });
+
+      const hash = hx.wait()
+      console.log(hash);
+
+      swal("Create Pool Transaction is in Process", "Please Follow Metamask ");
+    } else {
+      swal("TokenA Amount  and TokenB Amount Should be same ");
+    }
+  }
   return (
     <>
       <Navigationbar />
-      <Container className="h-100">
-
-
-
+      <Container className="h-100 pool">
         <Row>
-          <Col size='6' className="mx-auto my-auto">
-            <H1 color={colors.primary}>Public Pool</H1>
-            <Row className='flex-column mb-3'>
-              <Col className='mt-3'><H3>• Anyone can add liquidity</H3></Col>
-              <Col className='mt-3'><H3>• Parameters cannot be modified after creation</H3></Col>
-            </Row>
-            <H1 color={colors.primary}>Standard</H1>
-            <Row className='flex-column'>
-              <Col className='mt-3'><H3>• 50/50 value liquidity provision (same as Uniswap)</H3></Col>
-              <Col className='mt-3'><H3>• More parameters can be set</H3></Col>
-            </Row>
-          </Col>
-          <Col size='6' className='pt-5'>
-            <Form className="px-4 py-4 my-5">
+          <Col size='12' className='pt-5'>
+            <Form className="px-4 my-5">
               <Row>
-                <Col size='12' className="my-2">
-                  <Title variant={'dark'} value={'Create a pool'} ></Title>
+                <Col size='12' className="my-2 create-pool-title">
+                  <H2>Create Pool</H2>
                 </Col>
-                <Col size='12' className="my-2">
-                  <Row className='mb-2'><B1 className="form-check-label">01 Choose Pool Type</B1></Row>
-                  <ButtonGroupRadio className='col-md-12'
-                    name='publicPool' checked='Public Pool'
-                    disabled={['Private Pool', 'Pegged Pools']}
-                    values={['Public Pool', 'Private Pool', 'Pegged Pools',
-                    ]} />
-                </Col>
-                <Col size='12' className="my-2">
-                  <Row className='mb-2'><B1 className="form-check-label">02 Choose Pool Template</B1></Row>
-                  <ButtonGroupRadio className='col-md-12'
-                    name='peggedPool' checked='Standard'
-                    disabled={['Single Token', 'Custom']}
-                    values={['Standard', 'Single Token', 'Custom',
-                    ]} />
-                </Col>
+                <Row>
+                  <Col size='1' />
+                  <Col size='10' className="my-2">
+                    <Col size='12'>
+                      <Col className="mb-22">
+                        <H3>Select Pair</H3>
+                      </Col>
+                      <Row>
+                        <Col>
+                          <StyledDropBtn className="btn dropdown-toggle" type="button"
+                            data-bs-target="#tokenModal" data-bs-toggle="modal"
+                            aria-expanded="false" style={{ width: '100%', borderRadius: 4 }}>
+                            <div className='create-pool-tokenDropdown'>
 
-                <Col size='12' className="my-2">
-                  <DropdownInput
-                    label='Quote Token Amount'
-                    searchable={true}
-                    items={dtoken}
-                    onSelect={TokenAaddressA}
-                    onToken={amountA}
-                    value={{ title: 'Token A' }}
-                    inputType='number'
-                    placeholder='0'
-                    className="mb-4 mt-4"
-                  />
-                  <Row><B1 className="text-center">+</B1></Row>
-                  <TokenDrop
-                    label='Base Token Amount'
-                    searchable={true}
-                    items={values}
-                    onSelect={TokenAaddressB}
-                    onToken={amountB}
-                    value={{ title: 'Token B' }}
-                    inputType='number'
-                    placeholder='0'
-                    className='mb-4'
-                    disabled='disabled'
-                  />
-                </Col>
-                <Col size='12' className="my-2">
+                              {tokenOne ? String(tokenOne['tokenName']).split(' ')[0].trim() : 'Select a token'}
+                            </div>
+                          </StyledDropBtn>
+                        </Col>
+                        <Col>
+                          <StyledDropBtn className="btn dropdown-toggle" type="button"
+                            data-bs-target="#tokenModalTwo" data-bs-toggle="modal"
+                            aria-expanded="false" style={{ width: '100%', borderRadius: 4 }}>
+                            <div className='create-pool-tokenDropdown'>
 
-                  {/* <Row>
-                    <Col size='3' className="my-auto">
-                      <B1 className="" >Amount </B1>
+                              {tokenTwo ? String(tokenTwo['symbol']).split(' ')[0].trim() : 'Select a token'}
+                            </div>
+                          </StyledDropBtn>
+                        </Col>
+                      </Row>
                     </Col>
-                    <Col size="9">
-                      <FormInput className="w-100" placeholder="Token1 Minimum" />
+                    <Col size='12' className='depositeAmount'>
+                      <Col className="mb-22">
+                        <H3>Deposite Amount</H3>
+                      </Col>
+                      <Col>
+                        {tokenOne ? (
+                          <div className='depositeAmount-group'>
+                            <div className='selectedToken-div'>
+
+                              {tokenOne && (String(tokenOne['tokenName']).split(' ')[0].trim())}
+                            </div>
+                            <input value={tokenOne['tokenaddress']} onChange={(e) => setTokenOne(e.target.value)} type={"hidden"} className='depositeAmount-input' />
+                            <input onChange={(e) => setTokenOneAmount(e.target.value)} placeholder='0.0' type={"number"} className='depositeAmount-input' />
+                          </div>
+                        ) : (
+                          <div className='select-a-token'>select a Token</div>
+                        )}
+                      </Col>
+                      <Col>
+                        {tokenTwo ? (
+                          <div className='depositeAmount-group'>
+                            <span className='selectedToken-div'>
+
+                              {tokenTwo && (String(tokenTwo['symbol']).split(' ')[0].trim())
+
+                              }
+                            </span>
+                            <input value={tokenTwo['id']} onChange={(e) => setTokenTwo(e.target.value)} type={"hidden"} className='depositeAmount-input' />
+                            <input onChange={(e) => setTokenTwoAmount(e.target.value)} placeholder='0.0' type={"number"} className='depositeAmount-input' />
+                          </div>
+                        ) : (
+                          <div className='select-a-token'>select a Token</div>
+                        )}
+                      </Col>
+                      <Col> <Button
+                        className="row justify-content-center mt-2 mb-2 btnCreatePool"
+                        label={'Create Pool'}
+                        primary
+                        onClick={handleCreatePool}
+                      /></Col>
+
                     </Col>
-                  </Row> */}
-                  <br />
-                  <Row>
-                    {/* <Col size='3' className="my-auto">
-                      <B1 className="" >Amount </B1>
+                  </Col>
+                  <Col size='1' />
+                  {/* <Col size='4' className="my-2">
+                    <Col className="mb-22">
+                      <H3>Set Price Range</H3>
                     </Col>
-                    <Col size="9">
-                      <FormInput className="w-100" placeholder="Token2 Minimum" />
-                    </Col> */}
-                  </Row>
-                </Col>
-              </Row>
-              <Row className="justify-content-center mx-5 mt-3 pt-3">
-                <Button onClick={createPool} type="button" label={'Create Pool'} primary />
+                    <PriceCounter
+                      label='Min Price'
+                      subLabel='per ETH'
+                      value={minValue}
+                      onDecrease={() => {
+                        if (minValue != 1) {
+                          setMinValue(minValue - 1)
+                        }
+                      }}
+                      onIncrease={() => setMinValue(minValue + 1)}
+                    />
+                    <PriceCounter
+                      label='Max Price'
+                      subLabel='per ETH'
+                      value={maxValue}
+                      onDecrease={() => {
+                        if (maxValue != 1) {
+                          setMaxValue(maxValue - 1)
+                        }
+                      }}
+                      onIncrease={() => setMaxValue(maxValue + 1)}
+                    />
+                    <Button
+                      className="row justify-content-center mt-2 mb-2 btnCreatePool"
+                      label={'Create Pool'}
+                      primary
+                      onClick={handleCreatePool}
+                    />
+                  </Col> */}
+                </Row>
               </Row>
             </Form>
           </Col>
         </Row>
       </Container>
       <BottomBar />
+      <TokenModal id='tokenModal' standardTokens={values} onSelection={(item: any) => {
+        setTokenOne(item)
+      }} />
+      <TokenModal1 id='tokenModalTwo' standardTokens={dtoken} onSelection={(item: any) => {
+        setTokenTwo(item)
+      }} />
     </>
   )
 }
